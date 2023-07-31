90e6e6dad79365d7b676325e4be99892c07e021c
macbookpro <yangming.wu@my.jcu.edu.au>
2023-07-31 17:59:08 +0800

    List of numbers and usernames

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,18 @@
+numbers = []
+for i in range(5):
+    number = int(input("Number: "))
+    numbers.append(number)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+user_name = input("Your user name: ")
+if user_name in usernames:
+    print(f"Access granted")
+else:
+    print(f"Access denied")
