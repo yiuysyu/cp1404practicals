5d4a96ac72419f35eeb3726d409d51a4bc9ce995
macbookpro <yangming.wu@my.jcu.edu.au>
2023-08-21 17:39:57 +0800

    Complete ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,19 @@
+"""
+Estimate: 40 minutes
+Actual:   45 minutes
+"""
+
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        reflection_str = "Reflection: true" if self.reflection else "Reflection: false"
+        return f"{self.name}, Typing: {self.typing} , {reflection_str}, Year: {self.year}"
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
